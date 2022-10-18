56e9e19c5bf1df9a72474e2980285bdae3762028
ffescara <firdan.fescara07@gmail.com>
2022-10-18 18:20:18 +0700

    first commit

---
+++ app/src/main/java/com/ffescara/belajarandroid1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ffescara.belajarandroid1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ffescara.belajarandroid1.ui.theme.BelajarAndroid1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BelajarAndroid1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("FFescara")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Selamat datang di aplikasi $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BelajarAndroid1Theme {
+        Greeting("FFescara")
+    }
+}
