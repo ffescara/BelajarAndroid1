ade34e294cf90f4c3d9297d230bf94fc7b74a5c6
ffescara <firdan.fescara07@gmail.com>
2022-11-01 15:26:44 +0700

    Revisi Tugas

--- app/src/main/java/com/ffescara/belajarandroid1/Tugas.kt
+++ app/src/main/java/com/ffescara/belajarandroid1/Tugas.kt
@@ -1,15 +1,16 @@
 package com.ffescara.belajarandroid1
 
 import android.annotation.SuppressLint
-import android.media.Image
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,38 +19,54 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.util.*
 
 class Tugas : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Column() {
+            Column(modifier = Modifier.fillMaxSize()) {
                 Row(Modifier.weight(1f)) {
-                    Column(Modifier.weight(1f)) {
-                        Bingkai(name = "SANA", grup = "TWICE", gambar = 0, warna = Color.Red)
-                    }
-                    Column(Modifier.weight(1f)) {
-                        Bingkai(name = "MOMO", grup = "TWICE", gambar = 1, warna = Color.Green)
-                    }
-                    Column(Modifier.weight(1f)) {
-                        Bingkai(name = "TZUYU", grup = "TWICE", gambar = 2, warna = Color.Yellow)
-                    }
+                    Bingkai(name = getString(R.string.Sana),
+                        grup = getString(R.string.Twice),
+                        gambar = R.drawable.sana,
+                        warna = Color.Red,
+                        mod = Modifier.weight(1f)
+                    )
+                    Bingkai(name = getString(R.string.Momo),
+                        grup = getString(R.string.Twice),
+                        gambar = R.drawable.momo,
+                        warna = Color.Green,
+                        mod = Modifier.weight(1f)
+                    )
+                    Bingkai(name = getString(R.string.Tzuyu),
+                        grup = getString(R.string.Twice),
+                        gambar = R.drawable.tzuyu,
+                        warna = Color.Yellow,
+                        mod = Modifier.weight(1f)
+                    )
                 }
                 Row(Modifier.weight(1f)) {
-                    Column(Modifier.weight(1f)) {
-                        Bingkai(name = "JIHYO", grup = "TWICE", gambar = 3, warna = Color(0xFFff852b))
-                    }
-                    Column(Modifier.weight(1f)) {
-                        Bingkai(name = "NAYEON", grup = "TWICE", gambar = 4, warna = Color(0xFFc791c7))
-                    }
-                    Column(Modifier.weight(1f)) {
-                        Bingkai(name = "DAHYUN", grup = "TWICE", gambar = 5, warna = Color(0xFF93831d))
-                    }
+                    Bingkai(name = getString(R.string.Jihyo),
+                        grup = getString(R.string.Twice),
+                        gambar = R.drawable.jihyo,
+                        warna = Color(0xFFff852b),
+                        mod = Modifier.weight(1f)
+                    )
+                    Bingkai(name = getString(R.string.Nayeon),
+                        grup = getString(R.string.Twice),
+                        gambar = R.drawable.nayeon,
+                        warna = Color(0xFFc791c7),
+                        mod = Modifier.weight(1f)
+                    )
+                    Bingkai(name = getString(R.string.Dahyun),
+                        grup = getString(R.string.Twice),
+                        gambar = R.drawable.dahyun,
+                        warna = Color(0xFF93831d),
+                        mod = Modifier.weight(1f)
+                    )
                 }
             }
         }
@@ -58,32 +75,27 @@ class Tugas : ComponentActivity() {
 
 @SuppressLint("ResourceType")
 @Composable
-fun Bingkai(name: String, grup: String, gambar: Int, warna: Color) {
-    val images = intArrayOf(
-        R.drawable.sana,
-        R.drawable.momo,
-        R.drawable.tzuyu,
-        R.drawable.jihyo,
-        R.drawable.nayeon,
-        R.drawable.dahyun,
-    )
+fun Bingkai(name: String, grup: String, gambar: Int, warna: Color, mod: Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
+        modifier = mod
+            .fillMaxSize()
             .background(warna)
     ) {
         Image(
-            painter = painterResource(id = images[gambar]),
+            painter = painterResource(id = gambar),
             contentDescription = "Gambar $name",
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(100.dp)
                 .clip(CircleShape)
+                .border(
+                    BorderStroke(width = 2.dp,color = Color.White),
+                    CircleShape
+                )
         )
-        Text(text = "$name", modifier = Modifier.padding(0.dp,15.dp,0.dp,0.dp))
+        Text(text = "$name", modifier = Modifier.padding(0.dp, 15.dp, 0.dp, 0.dp))
         Text(text = "$grup", fontSize = 12.sp)
     }
 }
