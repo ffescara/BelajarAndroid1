3fd48d33bf2a4d7f425e20e1621766d9a99e82e9
ffescara <firdan.fescara07@gmail.com>
2022-11-03 09:34:52 +0700

    Lazy Column

---
+++ app/src/main/java/com/ffescara/belajarandroid1/model/Motivation.kt
@@ -0,0 +1,9 @@
+package com.ffescara.belajarandroid1.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Motivation(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val textResourceId: Int
+)
